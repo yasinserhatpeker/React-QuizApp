0f376c930464a1d01588e8966737fdd0ed7b2f3f
Yasin <yasinpeker43@gmail.com>
2025-04-26 09:52:08 +0300

    Rewritten the states of Quiz and question.js is added to Quiz component.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import questions from "../questions";
 
 export default function Quiz() {
-const [activeQuestionIndex,setActiveQuestionIndex]=useState(0);
-const [questionAnswers,setQuestionAnswers]=useState([]);
+const [questionAnswers,setQuestionAnswers]=useState(questions);
 
+      
+  const activeQuestionIndex=questionAnswers.length
     return <p>Currently Active Question</p>
 }
