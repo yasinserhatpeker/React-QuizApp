2e0beb6ae0a7e078d1b9c0e80dd86dd237043283
Yasin <yasinpeker43@gmail.com>
2025-04-26 10:54:25 +0300

    The options are shuffled in Quiz component.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,22 +5,27 @@ export default function Quiz() {
 const [questionAnswers,setQuestionAnswers]=useState([]);
 
 const activeQuestionIndex=questionAnswers.length;
+const shuffledAnswers=[...QUESTIONS[activeQuestionIndex].answers];
+shuffledAnswers.sort(() => Math.random() - 0.5);
 
  function handleSelect(selectedAnswer) {
     setQuestionAnswers((prevUserAnswers)=>{
        return [...prevUserAnswers,selectedAnswer];
     });
  }
-  
-
-    return <div id="question">
+  return ( 
+    <div id="quiz">
+        <div id="question">
          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
          <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map((answer)=> ( 
+            {shuffledAnswers.map((answer)=> ( 
                  <li key={answer} className="answer">
                     <button onClick={()=>handleSelect(answer)}>{answer}</button>
                  </li>
             ))}
          </ul>
     </div>
+    </div>
+
+    )
 }
